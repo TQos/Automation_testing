fbc98f5e60342f97b462194ce155d467a0c2fc69
TQos <Tomasz.Klos@gds.ey.com>
2023-12-21 14:02:54 +0100

    Hello world 1st - all 3 versions

--- Beata/src/Main_Beata.java
+++ Beata/src/Main_Beata.java
@@ -1,10 +1,10 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Main_Beata {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome Tomek!");
+        System.out.print("Hello and welcome Beata!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
